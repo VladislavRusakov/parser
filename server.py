8514dc4a8b1e445312eaacd7107bfe80fb9e84d0
Vlad <ruzikvlad5@yandex.ru>
2021-09-05 15:51:38 +0300

    final commit

--- server.py
+++ server.py
@@ -1,36 +1,31 @@
 import codecs
 import re
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import Response
+from fastapi.templating import Jinja2Templates
 
 
+TEMPLATES = Jinja2Templates(directory='templates')
 app = FastAPI()
 
 
 @app.get("/")
 def index_page():
     """Рендер главной страницы"""
-    with open('templates/index.html', 'r') as f:
-        index_page = f.read()
-    return Response(index_page, media_type='text/html')
-
+    return TEMPLATES.TemplateResponse('index.html') 
 
 
 @app.post("/read")
 async def read_file(input_file: UploadFile = File(...)):
     """Принимает файл из формы и читает его."""
-    
-    if input_file:
-        file_content = await input_file.read()
-        file_content = codecs.decode(file_content, encoding='utf-8', errors='replace')
-        return {"Word":"TF"}, file_parse(file_content)
-    else:
-        return Response(404, media_type='text/')
+
+    file_content = await input_file.read()
+    file_content = codecs.decode(file_content, encoding='utf-8', errors='replace')
+    return {"Word":"TF"}, file_parse(file_content)
+
     
 
 def file_parse(file: str):
     """Формирует список 50 самых частых слов в тексте."""
-    print(file)
     input_string = ' '.join(file.split()).replace('\n', '').lower()
     data = re.sub(r'[^\w\s]', '', input_string)
     data = re.sub(r'\s\w{1,2}\s', ' ', data).split(" ")   
@@ -40,7 +35,7 @@ def file_parse(file: str):
 
     # Считаем словом всё, что не меньше 2 символов.
     for word in reference:
-        if len(word) < 2 or str(word).isdigit():
+        if len(word) < 2:
             reference.remove(word)
 
     dic = {}
